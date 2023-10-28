cb58d190a6deb7036f24bf5e4524bd84d71fe352
nuhptr <nugrahaadi733@gmail.com>
2023-10-28 13:33:43 +0700

    _auth: layout and SignUpForm

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,5 +1,27 @@
+import { Outlet, Navigate } from "react-router-dom"
+
 const AuthLayout = () => {
-   return <div>AuthLayout</div>
+   const isAuthenticated = false
+
+   return (
+      <>
+         {isAuthenticated ? (
+            <Navigate to="/" />
+         ) : (
+            <>
+               <section className="flex flex-col items-center justify-center flex-1 py-10">
+                  <Outlet />
+               </section>
+
+               <img
+                  src="/assets/images/side-img.svg"
+                  alt="Logo"
+                  className="hidden object-cover w-1/2 h-screen bg-no-repeat xl:block"
+               />
+            </>
+         )}
+      </>
+   )
 }
 
 export default AuthLayout
