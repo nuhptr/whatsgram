3d7126fa9c3ad853312a4021e425c10b0db9d409
nuhptr <nugrahaadi733@gmail.com>
2023-10-28 11:57:32 +0700

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,12 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
-export default {
-   content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-      "./app/**/*.{ts,tsx}",
-      "./pages/**/*.{ts,tsx}",
-      "./components/**/*.{ts,tsx}",
-   ],
+// const defaultTheme = require("tailwindcss/defaultTheme")
+
+module.exports = {
+   darkMode: ["class"],
+   content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
    theme: {
-      container: {
-         center: true,
-         padding: "2rem",
-         screens: {
-            "2xl": "1400px",
-         },
-      },
+      container: { center: true, padding: "2rem", screens: { "2xl": "1400px" } },
       extend: {
          colors: {
             "primary-500": "#877EFF",
@@ -42,14 +34,8 @@ export default {
             inter: ["Inter", "sans-serif"],
          },
          keyframes: {
-            "accordion-down": {
-               from: { height: 0 },
-               to: { height: "var(--radix-accordion-content-height)" },
-            },
-            "accordion-up": {
-               from: { height: "var(--radix-accordion-content-height)" },
-               to: { height: 0 },
-            },
+            "accordion-down": { from: { height: 0 }, to: { height: "var(--radix-accordion-content-height)" } },
+            "accordion-up": { from: { height: "var(--radix-accordion-content-height)" }, to: { height: 0 } },
          },
          animation: {
             "accordion-down": "accordion-down 0.2s ease-out",
@@ -57,5 +43,5 @@ export default {
          },
       },
    },
-   plugins: [import("tailwindcss-animate")],
+   plugins: [require("tailwindcss-animate")],
 }
