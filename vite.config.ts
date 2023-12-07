8d9e36889171e5a762a28f384c98007a04c3a774
Adi Nugraha Putra <nugrahaadi733@gmail.com>
2023-12-07 15:00:35 +0700

    vite/aliases config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
-import path from "path"
-
 import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import react from "@vitejs/plugin-react-swc"
+
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
    plugins: [react()],
+   //* For aliases import
    resolve: {
-      // for shadcn import
-      alias: { "@": path.resolve(__dirname, "./src") },
+      alias: {
+         "@": path.resolve(__dirname, "./src"),
+      },
    },
 })
